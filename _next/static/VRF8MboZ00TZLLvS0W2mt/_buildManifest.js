9160b2b7c3eebd0e0062235828944737f837247e
jcrtr <36883870+jcrtr@users.noreply.github.com>
2022-08-05 10:29:32 +0000

    Deploying to gh-pages from @ jcrtr/borcy-site@3dbab409e7c2a7c1f0992ca2841aea596d1c348a 🚀

---
+++ _next/static/VRF8MboZ00TZLLvS0W2mt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b700860dce89a10d.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],"/tour/[url]":["static/chunks/pages/tour/[url]-3dcf4fd9a9db4abb.js"],sortedPages:["/","/_app","/_error","/tour/[url]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
