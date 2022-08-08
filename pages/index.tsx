e0d11b1c0cb939a45c739fae98723d6d81832607
likiblack <likiblack@gmail.com>
2022-08-08 13:33:36 +0300

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <div className={'min-h-screen bg-radial-at-t from-gray-700 via-gray-900 to-gray-900 py-12 flex flex-col items-center justify-center'}>
         <div className="lg:h-[300px] lg:max-w-screen-xl flex flex-wrap items-center justify-center mb-12">
             <div className={'flex justify-center'}>
-                <Image src={Logo}/>
+                <Image src={`/borcy-site/` + Logo}/>
             </div>
             <div className={'lg:px-12'}>
                 <h1 className={'font-bold text-white text-2xl py-2 text-center lg:text-left lg:mt-0 mt-8'}>Команда КВН</h1>
@@ -20,21 +20,21 @@ const IndexPage = () => (
             <div className={'grid lg:grid-cols-3 gap-12'}>
                 <Link href={'/vladikavkaz'}>
                     <img className={'rounded-large transform transition duration-500 hover:scale-110 cursor-pointer'}
-                         src={`6_VLADIKAVKAZ.jpg`}
+                         src={`/borcy-site/6_VLADIKAVKAZ.jpg`}
                          width={'400px'}
                          height={'100%'}
                          alt={''}/>
                 </Link>
                 <Link href={'/nalchik'}>
                     <img className={'rounded-large transform transition duration-500 hover:scale-110 cursor-pointer'}
-                         src={`7_NALCHIK.jpg`}
+                         src={`/borcy-site/7_NALCHIK.jpg`}
                          width={'400px'}
                          height={'100%'}
                          alt={''}/>
                 </Link>
                 <Link href={'/krasnodar'}>
                     <img className={'rounded-large transform transition duration-500 hover:scale-110 cursor-pointer'}
-                         src={`9_KRASNODAR.jpg`}
+                         src={`/borcy-site/9_KRASNODAR.jpg`}
                          width={'400px'}
                          height={'100%'}
                          alt={''}/>
